d012199e148f1009b98108db1f67258be6571310
VIR-WADWANI <vyw2001@hw.ac.uk>
2026-03-19 13:28:24 +0400

    Made the pgp certificate attributes in name_comment field be a JWT, converting the attributes into a JWT using the created SoA rsa key.
    In the PIP side, using the public key of SoA, the JWT is decoded and attributes extracted from it.
    
    Created 2 .pem files to store the pem format of the SoA public private keys, so as they can be shared with the PIP

--- PGPPIP.py
+++ PGPPIP.py
@@ -2,33 +2,20 @@ import gnupg
 import re
 import jwt
 import time
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
 
-## Generating RSA keys for PIP (currently acting as a Source of Authority for the access rights) - Different from GPG keys
-# Acts as trust anchor to be able to encrypt and send JWT to PDP and PDP can verify
-# Need to convert the keys into PEM format as JWT only recogises that format and cannot directly use the key.
+jwt_fields = ['sub', 'iss', 'iat', 'expiry', 'aud', 'nbf', 'jti']
 
-PIP_private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-PIP_public_key = PIP_private_key.public_key()
+# Getting the SoA public key pem from the .pem file
+with open("SoA_public_pem.pem", "rb") as f:
+    SoA_public_pem = f.read()
 
-PIP_private_pem = PIP_private_key.private_bytes( #pem format of private key
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption() #No encryption as JWT needs string
-)
-
-PIP_public_pem = PIP_public_key.public_bytes( #pem version of public key
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
 
 ## GPG - Key verification, attribute extraction
 
 gpg = gnupg.GPG(gnupghome="/Users/virwadwani/gnupgHome/gnupg_test",
                 options=["--pinentry-mode", "loopback"])
 
-fingerprint = "EE97CE68DEC613FA8C5BD9EDEDDB5721106F3554"
+fingerprint = "D031FE189631DB147B774C24B5699E4D4496B14A" 
 
 # Verify PGP key
 def verify_pgp(fingerprint):
@@ -44,12 +31,10 @@ def verify_pgp(fingerprint):
 
     #Add code for signature verification
 
-
     return True
 
 # Extracting attributes
 def getAttributes(fingerprint):
-    attributes = {}
     keys = gpg.list_keys(keys=[fingerprint])
     if not keys:
         print("key not found!!")
@@ -58,11 +43,16 @@ def getAttributes(fingerprint):
             print("UID not found!!")
         else:
             uid = keys[0]['uids'][-1] #UID = Name (key=Val;key=val) email
-            attr = re.search(r"\((.*?)\)", uid).group(1)
-            for a in attr.split(";"):
-                r, s = a.split("=",1)
-                attributes[r.strip()] = s.strip()
-    
+            attr = re.search(r"\(([^)]+)\)", uid).group(1)
+            attributes = jwt.decode(attr, SoA_public_pem, algorithms="RS256", audience="pip")
+            
+            if attributes['expiry'] and attributes['expiry'] > int(time.time()):
+                print("JWT token expired!")
+                return None
+            
+            for field in jwt_fields:
+                attributes.pop(field, None)
+
         return attributes
 
 if verify_pgp(fingerprint): #To verify key before extracting attributes
@@ -70,30 +60,5 @@ if verify_pgp(fingerprint): #To verify key before extracting attributes
 else:
     print("Key not verified!")
 
-## JWT encoding
-
-def create_jwt(fingerprint, attributes, private_key_pem):
-    payload = {
-        "sub": fingerprint,
-        "iss": "PGPPIP", #issuer
-        "iat": int(time.time()), #issued at time
-        "expiry": int(time.time() + 300) #Expiry time, set to 5 minutes from issuing
-    }
-
-    payload.update(attributes)
-
-    token = jwt.encode(payload, private_key_pem, algorithm="RS256")
-    return token
-
-if attributes:
-    jwt_attributes = create_jwt(fingerprint, attributes, PIP_private_pem)
-    print(jwt_attributes)
-
-# For verifying and extracting information from JWT - ideally done on PDP side
-def decode_jwt(token, public_key_pem):
-    decoded_jwt = jwt.decode(token, public_key_pem, algorithms="RS256")
-    return decoded_jwt
-
 print()
-if jwt_attributes:
-    print(decode_jwt(jwt_attributes, PIP_public_pem))
+print(attributes)
--- exeperiment.py
+++ exeperiment.py
@@ -1,4 +1,49 @@
 import gnupg
+import jwt
+import time
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+## Generating RSA keys for PIP (currently acting as a Source of Authority for the access rights) - Different from GPG keys
+# Acts as trust anchor to be able to encrypt and send JWT to PDP and PDP can verify
+# Need to convert the keys into PEM format as JWT only recogises that format and cannot directly use the key.
+
+SoA_private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+SoA_public_key = SoA_private_key.public_key()
+
+SoA_private_pem = SoA_private_key.private_bytes( #pem format of private key
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption() #No encryption as JWT needs string
+)
+
+SoA_public_pem = SoA_public_key.public_bytes( #pem version of public key
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Writing the pems to a .pem so can be accessed by PIP later from these files
+with open("SoA_private_pem.pem","wb") as f:
+    f.write(SoA_private_pem)
+
+with open("SoA_public_pem.pem","wb") as f:
+    f.write(SoA_public_pem)
+
+def create_jwt(token_subject, attributes, private_key_pem):
+    payload = {
+        "sub": token_subject,
+        "iss": "SoA", #issuer
+        "iat": int(time.time()), #issued at time
+        "expiry": int(time.time() + 300), #Expiry time, set to 5 minutes from issuing
+        "aud": "pip"
+    }
+
+    payload.update(attributes)
+
+    token = jwt.encode(payload, private_key_pem, algorithm="RS256")
+    return token
+
+# Creating pgp certificate using gpg and jwt
 
 gpg = gnupg.GPG(gnupghome="/Users/virwadwani/gnupgHome/gnupg_test",
                 options=["--pinentry-mode", "loopback"])
@@ -9,7 +54,7 @@ input_data = gpg.gen_key_input(
     key_type="RSA", 
     key_length=2048,
     passphrase = "123456",
-    name_comment= "role=employee;department=admin",
+    name_comment= create_jwt('attribute_token',{"role":"employee","department":"admin"},SoA_private_pem),
     expire_date= "1d",
     )
 
@@ -45,17 +90,17 @@ print()
 print()
 
 keys = gpg.list_keys()
-print(keys)
+#print(keys)
 
-'''for key in keys:
+for key in keys:
     for uid in key['uids']:
         print(uid)
     print(key['type'])
-'''
+
 '''for key in keys:
     gpg.delete_keys(key['fingerprint'], secret=True, passphrase = "123456")
-    gpg.delete_keys(key['fingerprint'])'''
-
+    gpg.delete_keys(key['fingerprint'])
+'''
 '''# Generating Extra Names without generating a new key:
 # Fingerprint of the key you want to modify
 key_fingerprint = 'YOUR_KEY_FINGERPRINT_HERE'
